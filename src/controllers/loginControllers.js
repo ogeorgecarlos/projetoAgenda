acdb7d096731a9ecefece340f20511232b069574
george gomes <georgecarlos@live.com>
2024-08-19 14:29:22 +0100

    inclusão do comando req.session.destroy() após logout.

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -1,34 +1,21 @@
-//Modulo de controller para a rota "login"´
+const Login = require("../models/loginModel") 
 
-
-const Login = require("../models/loginModel") //importacao do modulo login model que contem a logica de criação de usuarios
-
-exports.index = (req, res) =>{ //lógica de rendenrizacao para a rota /login/index
+exports.index = (req, res) =>{ 
     res.render(`login`)
 };
 
-exports.register = async (req, res) => { //logica de renderizacao para a rota /login/register
+exports.register = async (req, res) => {
     try{
         const body = req.body
-        const login = new Login(body) //instanciamos a classe login pertencente ao modulo "loginModel", que tem a finalidade de registar e processar a logica de criação de um novo usuário
-        await login.register(); //Chamamento do método principal que regista novos usuario em banco de dados.
+        const login = new Login(body)
+        await login.register();
         if(login.errors.length > 0){
-            //Se após o chamanmento d método register, houverem erros no array error da classe login
-            //Então, savaremos um nova mensagem flash com o array de erros para feedback ao cliene.
             req.flash("errors", login.errors)
-
-            //Após a mensagem de erro, redirecionamos o cliente para a página anterior
-            //Mas antes do redirecionamento, por garantia salvamos a sessao.
-            //Então, salvamos e como callbak da funcao save, redirecionamos o cliente.
-            req.session.save(function(){
-                return res.redirect("/login-index")
-            });
+            req.session.save(() => res.redirect("/login-index"));
             return
         }
         req.flash("success", "Usuário cadastrado com sucesso!")
-        req.session.save(()=>{
-            res.redirect("/login-index")
-        })
+        req.session.save(() => res.redirect("/login-index"))
     }catch(e){
         console.log(e)
         res.render("404")
@@ -36,27 +23,20 @@ exports.register = async (req, res) => { //logica de renderizacao para a rota /l
 };
 
 exports.userLogin = async (req, res) => {
-    //devo excluir esse comentário
     try{
-        if(!req.session.userMail) req.session.userMail = req.body.email
-        const body = req.body
-        const login = new Login(body)
+        req.session.userMail = req.body.email
+        const login = new Login(req.body, req.session.userMail)
         await login.enter()
 
         if(login.errors.length > 0){
             req.flash("errors", login.errors)
-            //devo excluir esse comentario tambem
-            req.session.save(function(){
-                return res.redirect("/login-index")
-            });
+            req.session.save(() => res.redirect("/login-index"));
             return
         }
 
         req.flash("success", "Você entrou no sistema")
         req.session.user = login.user
-        req.session.save(function (){
-            return res.redirect("/")
-        })
+        req.session.save(() => res.redirect("/"))
 
     }catch(e){
         console.log(e)
@@ -65,6 +45,6 @@ exports.userLogin = async (req, res) => {
 }
 
 exports.userLogout = (req, res) => {
-    req.session.user = null
+    req.session.destroy()
     res.redirect("/")
 }
